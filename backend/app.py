4cb4cba724a96f3566c4b49fff13298ceef73a46
saharhash <saharhashai@gmail.com>
2023-08-04 11:11:37 -0700

    Fixed corrupted image file encoding

--- backend/app.py
+++ backend/app.py
@@ -10,18 +10,44 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
+def clean_image_data(image_data):
+    # Transform the input byte array into a list for easier manipulation
+    byte_list = list(image_data)[2:] # remove unnecessary prefix (b')
+
+    # Define the sequence to truncate after
+    end_sequence = [0xFF, 0xD9]
+
+    # Check if the sequence is in the byte array
+    if end_sequence[0] in byte_list:
+        # Find the first index of the end sequence
+        index = byte_list.index(end_sequence[0])
+
+        # Check if the sequence is not at the end of the byte array
+        if index < len(byte_list) - 1:
+            # If the next byte matches the second part of the end sequence, truncate the byte array
+            if byte_list[index + 1] == end_sequence[1]:
+                byte_list = byte_list[:index + 2]
+
+    # Convert the list back to bytes
+    truncated_byte_array = bytes(byte_list)
+
+    return truncated_byte_array
+
 @app.route('/receive-img', methods=['POST'])
 def receive_image():
     image_data = request.files['file']
     dest_email = request.form.get('destEmail')
     image_style = request.form.get('imageStyle')
 
-    clean_image_data = image_data.read()[2:-3] # remove unnecessary prefix (b') and suffix (\r\n)
+    cleaned_image_data = clean_image_data(image_data.read())
     image_name = f'image_{datetime.now()}.jpeg'
-    byte_content = bytes(clean_image_data.decode('unicode_escape'), 'latin1')
+    byte_content = bytes(cleaned_image_data.decode('unicode_escape'), 'latin1')
     
     with open(image_name, 'wb') as f:
         f.write(byte_content)
+    
+    import subprocess
+    subprocess.call(['open', image_name])
 
     # image_path = generate_image_controlnet(image_name, image_style)
     # send_email(image_path, dest_email)
